5a807b8364a4e51d88f50441b6d697a0aefa6337
Piyush Kumar Jha <105655779+Piyush5784@users.noreply.github.com>
2025-01-01 16:49:37 +0530

    added insights

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { AboutSection } from "@/components/custom/About/about-section";
-import GitCalander from "@/components/custom/Footer";
+import Insights from "@/components/custom/Insights";
 import Sections from "@/components/custom/Sections";
 import { SelectItems } from "@/components/custom/select-items";
 import Skills from "@/components/custom/Skills";
@@ -9,6 +9,7 @@ export default function Home() {
     <>
       <div className="flex items-center justify-center flex-col p-4 ">
         <div className=" lg:w-[40%] rounded-lg pt-5 pb-10">
+          <Insights />
           <AboutSection />
           <Skills />
           <SelectItems />
---
+++ src/components/custom/Insights.tsx
@@ -0,0 +1,40 @@
+"use client";
+import { Check, CheckCheck } from "lucide-react";
+import React, { useState } from "react";
+
+const Insights = () => {
+  const [socket, setSocket] = useState<null | WebSocket>(null);
+  const [message, setMessage] = React.useState("");
+  const [users, setUsers] = React.useState("");
+  const id = "57846795";
+
+  React.useEffect(() => {
+    const newSocket = new WebSocket("wss://www.livestats.site?id=57846795");
+    setSocket(newSocket);
+    newSocket.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      if (data.type === "update") {
+        setUsers(data.totalUsers);
+      }
+    };
+    return () => {
+      newSocket.close();
+      setSocket(null);
+    };
+  }, []);
+
+  return (
+    <>
+      <div className="fixed bottom-0 right-0 mr-3">
+        <div className="border rounded-xl p-3 mb-10 bg-black">
+          <p className="dark:text-white">Current Users : {users}</p>
+          <div className="text-sm flex items-center justify-center">
+            <CheckCheck size={22} /> Verified by proof
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Insights;
