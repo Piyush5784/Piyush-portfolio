1ce0a48da4ad5103516b6ff367b0d5c900c1a518
Piyush5784 <piyushjha5668@gmail.com>
2025-01-12 23:38:34 +0530

    added insights

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,12 @@ import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-if (process.env.NODE_ENV === "development") {
-  await setupDevPlatform();
+async function setup() {
+  if (process.env.NODE_ENV === "development") {
+    await setupDevPlatform();
+  }
 }
+
+setup();
 
 export default nextConfig;
